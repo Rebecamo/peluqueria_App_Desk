4c8a828ab09f20237ad29510b5851c9b93c54323
EmilyVelasquez2204 <VF22006@ues.edu.sv>
2024-11-18 17:11:55 -0600

    Cambio en module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.peluqueria_app_desk {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
-
+  opens com.example.peluqueria_app_desk.Modelos to javafx.base;
     opens com.example.peluqueria_app_desk to javafx.fxml;
     exports com.example.peluqueria_app_desk;
 }
