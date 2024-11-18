204e14e5808dceb9b517305eb226a8a0c55f270f
EmilyVelasquez2204 <VF22006@ues.edu.sv>
2024-11-18 16:59:47 -0600

    Conexion a la base de datos

---
+++ src/main/java/com/example/peluqueria_app_desk/Conexion/ConexionDB.java
@@ -0,0 +1,22 @@
+package com.example.peluqueria_app_desk.Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    private static final String url="jdbc:postgresql://localhost:5432/db_R&AGlamStudio" +
+            "";
+    private static final String USER ="postgres";
+    private static final String PASS = "Evf22006";
+
+    public static Connection connection(){
+        try {
+            Connection conectar = DriverManager.getConnection(url, USER, PASS);
+            System.out.println("Conectado a la base");
+            return conectar;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
