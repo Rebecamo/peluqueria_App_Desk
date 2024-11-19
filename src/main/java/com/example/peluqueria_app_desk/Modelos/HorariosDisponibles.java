a24e3ee198baeaf4c6328538bea67da7f8b55f0c
EmilyVelasquez2204 <VF22006@ues.edu.sv>
2024-11-19 11:03:10 -0600

    Intento

---
+++ src/main/java/com/example/peluqueria_app_desk/Modelos/HorariosDisponibles.java
@@ -0,0 +1,13 @@
+package com.example.peluqueria_app_desk.Modelos;
+
+public class HorariosDisponibles {
+    public String tiempo;
+
+    public String getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(String tiempo) {
+        this.tiempo = tiempo;
+    }
+}
