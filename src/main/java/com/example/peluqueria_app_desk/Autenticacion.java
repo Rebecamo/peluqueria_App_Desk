2534f990c278a4537ac06452f3b22c8f0429a083
EmilyVelasquez2204 <VF22006@ues.edu.sv>
2024-11-22 14:10:25 -0600

    para iniciar sesion

---
+++ src/main/java/com/example/peluqueria_app_desk/Autenticacion.java
@@ -0,0 +1,35 @@
+package com.example.peluqueria_app_desk;
+
+
+import com.example.peluqueria_app_desk.Conexion.ConexionDB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Autenticacion {
+    private static int idEmpleadoLogueado; // ID del empleado autenticado
+
+    public boolean iniciarSesion(String correo, String contraseña) {
+        String consulta = "SELECT idempleado FROM tbl_empleados WHERE correo = ? AND contraseña = ?";
+        try (Connection conn = ConexionDB.connection();
+             PreparedStatement stmt = conn.prepareStatement(consulta)) {
+
+            stmt.setString(1, correo);
+            stmt.setString(2, contraseña);
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                idEmpleadoLogueado = rs.getInt("idempleado");
+                return true; // Inicio de sesión exitoso
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false; // Fallo en el inicio de sesión
+    }
+
+    public static int obtenerIdEmpleadoLogueado() {
+        return idEmpleadoLogueado;
+    }
+}
